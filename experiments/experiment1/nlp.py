c8088398fc5e43737fcfc9cd838607bf59f2d70f
Erin <erindb@stanford.edu>
2017-07-02 01:20:59 +0000

    make exportable function for NLP

--- experiments/experiment1/nlp.py
+++ experiments/experiment1/nlp.py
@@ -4,5 +4,12 @@
 Server will use this script to transform user responses
 to components:
 
+There is a sym link to this file. This file exports a single function:
+
+process_parsed_data
 
 """
+
+def process_parsed_data(parse):
+  return "hello"
+
